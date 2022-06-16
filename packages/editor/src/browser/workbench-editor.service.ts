31ed03323acc8bc65d355e45d83eb7e2189936d3
xinglong.wangwxl <xinglong.wangwxl@antgroup.com>
2022-06-16 17:23:41 +0800

    Revert "fix: quick change tab (#783)"
    This reverts commit f48213c107590087705115ad3b2a20ccd5c5ae5a.

--- packages/editor/src/browser/workbench-editor.service.ts
+++ packages/editor/src/browser/workbench-editor.service.ts
@@ -1438,7 +1438,6 @@ export class EditorGroup extends WithEventBus implements IGridEditorGroup {
 
       if (activeOpenType.type === 'code') {
         const documentRef = await this.getDocumentModelRef(resource.uri);
-        this.resolveTabChanged(_resource, this.currentResource);
         await this.codeEditorReady.onceReady(async () => {
           await this.codeEditor.open(documentRef);
 
@@ -1573,7 +1572,10 @@ export class EditorGroup extends WithEventBus implements IGridEditorGroup {
         return; // other type not handled
       }
 
-      this.resolveTabChanged(_resource, this.currentResource);
+      if (_resource !== this.currentResource) {
+        throw new EditorTabChangedError(); // 在打开过程中已经改变了
+      }
+
       this._currentOpenType = activeOpenType;
       this.notifyBodyChanged();
 
@@ -1585,13 +1587,6 @@ export class EditorGroup extends WithEventBus implements IGridEditorGroup {
       }
 
       this.cachedResourcesActiveOpenTypes.set(resource.uri.toString(), activeOpenType);
-    }
-  }
-
-  private resolveTabChanged(lastResource: IResource, curResource: MaybeNull<IResource>): void {
-    if (lastResource !== curResource) {
-      // 打开过程中改变了tab
-      throw new EditorTabChangedError();
     }
   }
 
