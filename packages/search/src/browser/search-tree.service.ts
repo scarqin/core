44c2bd0a9b68eb90c7f4fbf2073e66eb16633b83
yantze <yutian.yz@alibaba-inc.com>
2022-11-07 10:15:57 +0800

    fix: make the search references clean up
    # Conflicts:
    #	packages/editor/src/browser/workbench-editor.service.ts

--- packages/search/src/browser/search-tree.service.ts
+++ packages/search/src/browser/search-tree.service.ts
@@ -97,10 +97,9 @@ export class RangeHighlightDecorations implements IDisposable {
     if (this._model !== model) {
       this.clearModelListeners();
       this._model = model;
+
       this._modelDisposables.add(
         this._model.onDidChangeDecorations((e) => {
-          this._modelRef?.dispose();
-          this._modelRef = null;
           this.clearModelListeners();
           this.removeHighlightRange();
           this._model = null;
@@ -108,8 +107,6 @@ export class RangeHighlightDecorations implements IDisposable {
       );
       this._modelDisposables.add(
         this._model.onWillDispose(() => {
-          this._modelRef?.dispose();
-          this._modelRef = null;
           this.clearModelListeners();
           this.removeHighlightRange();
           this._model = null;
@@ -393,7 +390,7 @@ export class SearchTreeService {
       this.replaceDocumentModelContentProvider.delete(replaceURI);
     } else {
       const uri = new URI(result.fileUri);
-      await this.workbenchEditorService.open(new URI(result.fileUri), {
+      const openResourceResult = await this.workbenchEditorService.open(new URI(result.fileUri), {
         preview: isPreview,
         focus: !isPreview,
         range: {
@@ -404,10 +401,12 @@ export class SearchTreeService {
         },
       });
 
-      this.rangeHighlightDecorations.highlightRange(
-        uri,
-        new monaco.Range(result.line, result.matchStart, result.line, result.matchStart + result.matchLength),
-      );
+      if (openResourceResult && !openResourceResult.resource.deleted) {
+        this.rangeHighlightDecorations.highlightRange(
+          uri,
+          new monaco.Range(result.line, result.matchStart, result.line, result.matchStart + result.matchLength),
+        );
+      }
     }
   }
 
